feaad1155139b07dea07d6c7ac3e17d1ec8ba14d
liuwendi <1091010717@qq.com>
2018-04-05 09:50:32 -0700

    first compile pass

---
+++ include/config.h
@@ -0,0 +1,13 @@
+/* Automatically generated - do not edit */
+#define CONFIG_SYS_ARCH  "arm"
+#define CONFIG_SYS_CPU   "armv7"
+#define CONFIG_SYS_BOARD "zynq"
+#define CONFIG_SYS_VENDOR "v3best"
+#define CONFIG_SYS_SOC    "zynq"
+#define CONFIG_BOARDDIR board/v3best/zynq
+#include <config_cmd_defaults.h>
+#include <config_defaults.h>
+#include <configs/zynq_snowleo.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
+#include <config_uncmd_spl.h>
---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Apr 05 2018"
+#define U_BOOT_TIME "09:47:59"
