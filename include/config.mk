28d0042e062b52e9a2b825ef8faf1a13b15ef208
stulwd <stulwd@163.com>
2018-04-05 22:24:50 -0700

    ..

---
+++ include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = armv7
+BOARD  = zynq
+VENDOR = v3best
+SOC    = zynq
---
+++ include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2012.10-g6652a32"
+#define U_BOOT_VERSION "U-Boot 2012.10-g6652a32"
+#define CC_VERSION_STRING "arm-xilinx-linux-gnueabi-gcc (Sourcery CodeBench Lite 2011.09-50) 4.6.1"
+#define LD_VERSION_STRING "GNU ld (Sourcery CodeBench Lite 2011.09-50) 2.21.53.20110905"
